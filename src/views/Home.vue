da69050076aa82e08be234000cd1e84602fd5ae8
Pierrick Gicquelais <rico@MacBook-Pro-de-Pierrick.local>
2019-05-30 22:35:59 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Charme logo" src="../assets/charme.jpeg">
+
+    <h4>Patientez mes petits loulous ♥</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
